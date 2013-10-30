8c6215cef7eb7c400e8422faa090661d200b7971
Christian Moll <zero@lazyzero.de>
2013-10-30 20:55:00 +0100

    moving watermark into mediamanager
    Now the watermark.png has to be stored in :watermark.png or
    :wiki:watermark.png
    Files that are named watermark.png are excluded from watermarking, so
    that they can be uploaded by the mediamanger.

--- action/watermark.php
+++ action/watermark.php
@@ -36,57 +36,60 @@ class action_plugin_watermark_watermark extends DokuWiki_Action_Plugin {
      */
 
     public function handle_media_upload_finish(Doku_Event &$event, $param) {
-	$watermark = imagecreatefrompng(DOKU_PLUGIN.'watermark/watermark.png');
+	// get watermark either out of the template images folder or data/media folder
+    $watermarkSize = array();
+    $watermarkFile = tpl_getMediaFile(array(':wiki:watermark.png', ':watermark.png'), true, $watermarkSize);
+    $watermark = imagecreatefrompng(str_replace(':/','://',str_replace('//','/', $watermarkFile)));
+	
 	$sourcefile = $event->data[1];
 	$fileType = strtolower(substr($sourcefile, strlen($sourcefile)-3));
+	$isWatermark = strpos($sourcefile, 'watermark.png');
 
-	switch($fileType) {
-	    case('gif'):
-		$image = imagecreatefromgif($sourcefile);
-		break;
-	      
-	    case('png'):
-		$image = imagecreatefrompng($sourcefile);
-		break;
-	      
-	    default:
-		$image = imagecreatefromjpeg($sourcefile);
+	if ($isWatermark === false ) {
+		switch($fileType) {
+			case('gif'):
+			$image = imagecreatefromgif($sourcefile);
+			break;
+		  
+			case('png'):
+			$image = imagecreatefrompng($sourcefile);
+			break;
+		  
+			default:
+			$image = imagecreatefromjpeg($sourcefile);
+		}
+	
+		$watermark_width = imagesx($watermark);  
+		$watermark_height = imagesy($watermark);  
+	
+		$size = getimagesize($sourcefile);
+	
+		$dest_x = $size[0] - $watermark_width - 5;  
+		$dest_y = $size[1] - $watermark_height - 5;  
+	
+		imagesavealpha($image, true);
+		imagealphablending($image, true);
+		imagesavealpha($watermark, true);
+		imagealphablending($watermark, true);
+	
+		imagecopy($image, $watermark, $dest_x, $dest_y, 0, 0, $watermark_width, $watermark_height);
+	
+		switch($fileType) {
+	
+		  // remember we don't need gif any more, so we use only png or jpeg.
+		  // See the upsaple code immediately above to see how we handle gifs
+		  case('png'):
+			  imagepng($image, $sourcefile, 0);
+			  break; 
+		  default:
+			  imagejpeg($image, $sourcefile, 90);  
+		}        
+	
+
+		imagedestroy($image);  
+		imagedestroy($watermark);  
+		}
 	}
-	
-	$watermark_width = imagesx($watermark);  
-	$watermark_height = imagesy($watermark);  
-	
-	$size = getimagesize($sourcefile);
-	
-	$dest_x = $size[0] - $watermark_width - 5;  
-	$dest_y = $size[1] - $watermark_height - 5;  
-	
-	imagesavealpha($image, true);
-	imagealphablending($image, true);
-	imagesavealpha($watermark, true);
-	imagealphablending($watermark, true);
-	
-	imagecopy($image, $watermark, $dest_x, $dest_y, 0, 0, $watermark_width, $watermark_height);
-	
-	
-	switch($fileType) {
-    
-	  // remember we don't need gif any more, so we use only png or jpeg.
-	  // See the upsaple code immediately above to see how we handle gifs
-	  case('png'):
-	      imagepng($image, $sourcefile, 0);
-	      break; 
-	  default:
-	      imagejpeg($image, $sourcefile, 90);  
-	}        
-	
-
-	imagedestroy($image);  
-	imagedestroy($watermark);  
-	
-	var_dump($image);
-    }
-
 }
 
 // vim:ts=4:sw=4:et:
